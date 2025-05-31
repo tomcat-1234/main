cee56488eeaedaa386f0755ea14327c407a4c3f9
tomcat-1234 <dopolo6625@besibali.com>
2025-05-31 14:56:10 -0400

    Create pythonTest.py

---
+++ pythonTest.py
@@ -0,0 +1,18 @@
+
+
+
+class Node:
+
+  def __init__(this,arg):
+    pass:
+
+  def __call__(self,f):
+    print("Starting Wrapped Function")
+    f()
+    print("Completed Wrapped Function")
+
+
+
+def function1():
+  print("Production Function")
+  
